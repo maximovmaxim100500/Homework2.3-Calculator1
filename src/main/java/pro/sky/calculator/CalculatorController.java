2a1ced3a72e2f36a9f877fd3a380c3d6aae8d323
MaximovMaxim <maximovmaxim100500@gmail.com>
2023-12-20 01:42:52 +0300

    Домашка готова

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,10 +1,12 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping ("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
@@ -12,28 +14,28 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping(path = "/calculator")
+    @GetMapping()
     public String calculator() {
         return calculatorService.calculatorHello();
     }
 
-    @GetMapping(path = "/calculator/plus")
+    @GetMapping(path = "/plus")
     public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         return num1 + " + " + num2 + " = " + calculatorService.plus(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/minus")
+    @GetMapping(path = "/minus")
     public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/multiply")
+    @GetMapping(path = "/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
     }
 
 
-    @GetMapping(path = "/calculator/divide")
+    @GetMapping(path = "/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num2 == 0) {
             return "Ошибка! Делить на ноль нельзя!";
